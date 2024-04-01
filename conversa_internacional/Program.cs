e168052efe75ceacf5c16126a2da26e64bd25ca9
ALUNO <ALUNO@LABFPC17>
2024-04-01 07:55:27 -0300

    Adicionar arquivos de projeto.

---
+++ conversa_internacional/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int a, b, cont = 0;
+        string c = "", ca = "";
+
+        a = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < a; i++)
+        {
+
+            b = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < b; j++)
+            {
+                c = Console.ReadLine();
+                if (j == 0)
+                {
+                    ca = c;
+                }
+
+                if (ca == c)
+                {
+                    cont++;
+                }
+
+                ca = c;
+
+            }
+
+            if (cont == b)
+            {
+                Console.WriteLine(c);
+            }
+            else
+            {
+                Console.WriteLine("ingles");
+            }
+
+            cont = 0;
+
+        }
+        Console.ReadKey();
+
+    }
+}
